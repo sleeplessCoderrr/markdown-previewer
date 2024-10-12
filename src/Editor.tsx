ad2ddf8c446dd3f3d66860740c18169db6457b4e
Vincentius Oktara <108650831+Vincent1646@users.noreply.github.com>
2024-10-12 16:52:48 +0700

    setup

---
+++ src/Editor.tsx
@@ -0,0 +1,7 @@
+function Editor() {
+    return (
+        <div></div>
+    );
+}
+
+export default Editor;
---
+++ src/Previewer.tsx
@@ -0,0 +1,7 @@
+function Previewer(){
+    return (
+        <div></div>
+    );
+}
+
+export default Previewer;
